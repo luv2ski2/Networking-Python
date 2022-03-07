2da70d3b9452aba0a3bdd5b3231e925f814b5c34
Joseph Lev-Tov <jtlevtov@gmail.com>
2022-03-06 18:08:24 -0700

    This works?

--- server.py
+++ server.py
@@ -11,10 +11,12 @@ ADDR = SERVER, PORT
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind(ADDR)
+# s.bind(ADDR)
 
 def handleClient(conn, addr):
-    print(f"[NEW CONNECTION] {addr} has connected.")
+    # print(f"[NEW CONNECTION] {addr} has connected.")
+    sendThread = threading.Thread(target=handleSend, args=(conn))
+    sendThread.start()
 
     connected = True
     while connected:
@@ -27,15 +29,49 @@ def handleClient(conn, addr):
 
             print(f"{msg} from {addr}\n")
             conn.send("Message received".encode(FORMAT))
-    conn.close()
 
-def start():
+    conn.close()
+    sendThread.join()
+
+def handleSend(conn):
+    while True:
+        sendMsg = input()
+        send(sendMsg, conn)
+        if sendMsg == DISCONNECT_MESSAGE:
+            break
+
+def send(msg, conn):
+    message = msg.encode(FORMAT)
+    messageLen = len(message)
+    sendLen = str(messageLen).encode(FORMAT)
+    sendLen += b' ' * (HEADER - len(sendLen))
+    conn.send(sendLen)
+    conn.send(message)
+    print(s.recv(2048).decode(FORMAT))
+
+def connect():
+    serv = input("Who do you want to connect with?\n")
+    port = input("What port do you want to connect to?")
+    addr = serv, port
+    s.connect(addr)
+    thread = threading.Thread(target=handleSend, args=s)
+    thread.start()
+
+def start(server, port):
+    addr = server, port
+    s.bind(addr)
+    print(f"[Starting on {port} {server}")
     s.listen()
+    print("[Listening]")
     while True:
         conn, addr = s.accept()
         thread = threading.Thread(target=handleClient, args=(conn, addr))
         thread.start()
         print(f"\nACTIVE CONNECTIONS: {threading.activeCount() - 1}\n")
 
-print(f"Starting server on host: {SERVER}, port {PORT}")
-start()
+# print(f"Starting server on host: {SERVER}, port {PORT}")
+# SERVER = input("What server do you want to run on?\n")
+PORT = int(input("What port do you want to run on?\n"))
+# start()
+threadStart = threading.Thread(target=start, args=(SERVER, PORT))
+threadStart.start()
